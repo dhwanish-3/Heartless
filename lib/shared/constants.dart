d98cd32b98066b4beb7cb6b5ca0a7084def8d549
Shelly Anissa <shellyannissa29@gmail.com>
2023-10-29 11:41:18 +0530

    added assets and themes

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final primaryColor = const Color.fromARGB(1, 55, 244, 218);
+  final darkPrimaryColor = const Color.fromARGB(255, 0, 0, 0);
+}
