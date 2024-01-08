e34b28f6502deaff8d0f6aa5dcfd8f80c4699229
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-01-08 16:41:10 +0530

    Dummy home for testing + splash initial route

--- lib/backend/controllers/patient_controller.dart
+++ lib/backend/controllers/patient_controller.dart
@@ -4,19 +4,19 @@ import 'package:heartless/shared/provider/auth_notifier.dart';
 
 class PatientController with BaseController {
   final PatientAuth _patientAuth = PatientAuth();
-  void login(AuthNotifier authNotifier) async {
+  Future<void> login(AuthNotifier authNotifier) async {
     await _patientAuth.loginPatient(authNotifier).catchError(handleError);
   }
 
-  void signUp(AuthNotifier authNotifier) async {
+  Future<void> signUp(AuthNotifier authNotifier) async {
     await _patientAuth.signUpPatient(authNotifier).catchError(handleError);
   }
 
-  void logout(AuthNotifier authNotifier) async {
+  Future<void> logout(AuthNotifier authNotifier) async {
     await _patientAuth.logoutPatient(authNotifier).catchError(handleError);
   }
 
-  void googleSignIn(AuthNotifier authNotifier) async {
+  Future<void> googleSignIn(AuthNotifier authNotifier) async {
     await _patientAuth.googleSignIn(authNotifier).catchError(handleError);
   }
 }
