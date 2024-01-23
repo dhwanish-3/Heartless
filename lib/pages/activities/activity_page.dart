47c6b67f25807b9cd6ea4f1375ae8fc2627b196d
Sam-J-Francis <130873271+Sam-J-Francis@users.noreply.github.com.>
2024-01-23 22:50:06 +0530

    Started issue #44

---
+++ lib/pages/activities/activity_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ActivityPage extends StatefulWidget {
+  const ActivityPage({super.key});
+
+  @override
+  State<ActivityPage> createState() => _ActivityPageState();
+}
+
+class _ActivityPageState extends State<ActivityPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
