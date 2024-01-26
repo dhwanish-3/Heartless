8b813304504111f511d5562c163b5c4458f70eb5
Shelly Anissa <shellyannissa29@gmail.com>
2024-01-26 14:33:41 +0530

    multiTogglePanel wrapped with Provider

--- lib/widgets/activity/multi_toggle_panel.dart
+++ lib/widgets/activity/multi_toggle_panel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:heartless/shared/constants.dart';
+import 'package:heartless/main.dart';
+import 'package:heartless/shared/provider/widget_provider.dart';
 import 'package:heartless/widgets/auth/custom_text_button.dart';
 
 class MutltiToggle extends StatelessWidget {
@@ -7,49 +8,74 @@ class MutltiToggle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool emailPhoneToggle = true;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Container(
-      width: screenWidth * 0.7,
-      padding: const EdgeInsets.all(2),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.grey.withOpacity(0.4),
-      ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Expanded(
-          flex: 1,
-          child: CustomTextButton(
-            text: 'ALL',
-            containerWidth: screenWidth * 0.2,
-            isHighlighted: emailPhoneToggle,
-          ),
+
+    WidgetNotifier widgetNotifier =
+        Provider.of<WidgetNotifier>(context, listen: false);
+
+    return Consumer<WidgetNotifier>(builder: (context, value, child) {
+      return Container(
+        width: screenWidth * 0.7,
+        padding: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.grey.withOpacity(0.4),
         ),
-        Expanded(
-          flex: 1,
-          child: CustomTextButton(
-            text: 'MED',
-            containerWidth: screenWidth * 0.2,
-            isHighlighted: !emailPhoneToggle,
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () {
+                widgetNotifier.changeToggleSelection(0);
+              },
+              child: CustomTextButton(
+                text: 'ALL',
+                containerWidth: screenWidth * 0.2,
+                isHighlighted: widgetNotifier.toggleSelectionIndex == 0,
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          flex: 1,
-          child: CustomTextButton(
-            text: 'DIET',
-            containerWidth: screenWidth * 0.2,
-            isHighlighted: !emailPhoneToggle,
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () {
+                widgetNotifier.changeToggleSelection(1);
+              },
+              child: CustomTextButton(
+                text: 'MED',
+                containerWidth: screenWidth * 0.2,
+                isHighlighted: widgetNotifier.toggleSelectionIndex == 1,
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          flex: 1,
-          child: CustomTextButton(
-            text: 'DRILL',
-            containerWidth: screenWidth * 0.2,
-            isHighlighted: !emailPhoneToggle,
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () {
+                widgetNotifier.changeToggleSelection(2);
+              },
+              child: CustomTextButton(
+                text: 'DIET',
+                containerWidth: screenWidth * 0.2,
+                isHighlighted: widgetNotifier.toggleSelectionIndex == 2,
+              ),
+            ),
           ),
-        )
-      ]),
-    );
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () {
+                widgetNotifier.changeToggleSelection(3);
+              },
+              child: CustomTextButton(
+                text: 'DRILL',
+                containerWidth: screenWidth * 0.2,
+                isHighlighted: widgetNotifier.toggleSelectionIndex == 3,
+              ),
+            ),
+          ),
+        ]),
+      );
+    });
   }
 }
--- lib/widgets/auth/custom_text_button.dart
+++ lib/widgets/auth/custom_text_button.dart
@@ -14,14 +14,22 @@ class CustomTextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40,
+      height: 30,
       width: containerWidth,
       // padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        color: isHighlighted
-            ? Theme.of(context).primaryColor
-            : Theme.of(context).canvasColor,
+        color: isHighlighted ? Theme.of(context).primaryColor : null,
+        boxShadow: isHighlighted
+            ? [
+                const BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: Offset(1, 1), // changes position of shadow
+                ),
+              ]
+            : null,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -29,8 +37,8 @@ class CustomTextButton extends StatelessWidget {
           Text(
             text,
             style: TextStyle(
-              color: isHighlighted ? Colors.white : Colors.grey,
-              fontWeight: FontWeight.bold,
+              color: isHighlighted ? Colors.white : Colors.black,
+              fontWeight: FontWeight.w600,
               fontSize: 15,
             ),
           ),
