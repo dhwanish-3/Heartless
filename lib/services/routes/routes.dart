c12a67784c6bb408112a56814db94f8f7a2afed9
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-02-02 22:31:31 +0530

    Fixed & finished testing connencting patient & nurse

--- lib/services/routes/routes.dart
+++ lib/services/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:heartless/pages/auth/choose_user_type.dart';
 import 'package:heartless/pages/auth/create_password.dart';
 import 'package:heartless/pages/auth/dummy_home.dart';
+import 'package:heartless/pages/auth/dummy_nurse_home.dart';
 import 'package:heartless/pages/auth/forgot_password.dart';
 import 'package:heartless/pages/auth/splash_screen.dart';
 import 'package:heartless/pages/auth/login_page.dart';
@@ -37,6 +38,12 @@ class Routes {
       // ! Patient routes ! //
       case RouteNames.patientHome:
         return MaterialPageRoute(builder: (_) => const DummyHome());
+
+      // ! Nurse routes ! //
+      case RouteNames.nurseHome:
+        return MaterialPageRoute(builder: (_) => const DummyNurseHome());
+
+      // todo: add doctor routes
       default:
         return MaterialPageRoute(builder: (_) => Container());
     }
