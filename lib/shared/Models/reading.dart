d3a9459d1203d0fb0c1c201af758e06718b64abf
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-02-16 23:26:01 +0530

    Reading model + services + controller + date services

---
+++ lib/shared/Models/reading.dart
@@ -0,0 +1,52 @@
+enum ReadingType {
+  heartRate,
+  bodyTemperature,
+  bloodPressure,
+  bloodOxygen,
+  weight,
+  height,
+  glucose,
+  cholesterol,
+  other
+}
+
+class Reading {
+  String id = '';
+  DateTime time = DateTime.now();
+  String value = '';
+  String unit = '';
+  String comments = '';
+  ReadingType type = ReadingType.heartRate;
+  String patientId = '';
+
+  Reading(
+      {required this.id,
+      required this.time,
+      required this.value,
+      required this.unit,
+      required this.comments,
+      required this.type,
+      required this.patientId});
+
+  Reading.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    time = map['time'].toDate(); // Convert Timestamp to DateTime
+    value = map['value'];
+    unit = map['unit'];
+    comments = map['comments'];
+    type = ReadingType.values[map['type']];
+    patientId = map['patientId'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'time': time, // Convert DateTime to Timestamp done by Firestore
+      'value': value,
+      'unit': unit,
+      'comments': comments,
+      'type': type.index,
+      'patientId': patientId,
+    };
+  }
+}
