3fc5ee39c9ad20b8ea5a216806708ef3b6546a95
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-03-08 14:58:16 +0530

    Internet check + dialog added + tested

--- lib/pages/auth/splash_screen.dart
+++ lib/pages/auth/splash_screen.dart
@@ -1,3 +1,6 @@
+import "dart:developer";
+
+import "package:connectivity_plus/connectivity_plus.dart";
 import "package:flutter/material.dart";
 import "package:flutter_svg/svg.dart";
 import "package:heartless/backend/services/notifications/notification_services.dart";
@@ -13,26 +16,81 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  Connectivity connectivity = Connectivity();
+  bool isAlertSet = false;
+  bool inSplashScreen = true;
 
   @override
   void initState() {
+    getConnectivity();
     super.initState();
+    connectivity.checkConnectivity().then((value) {
+      if (value == ConnectivityResult.none) {
+        showFormDialog();
+        isAlertSet = true;
+      } else {
+        moveForward();
+      }
+    });
+  }
+
+  void getConnectivity() async {
+    connectivity.onConnectivityChanged
+        .listen((ConnectivityResult result) async {
+      log(result.toString());
+      if (result == ConnectivityResult.none && !isAlertSet) {
+        showFormDialog();
+        isAlertSet = true;
+      }
+    });
+  }
+
+  void moveForward() {
     AuthNotifier authNotifier =
         Provider.of<AuthNotifier>(context, listen: false);
     Future.delayed(const Duration(seconds: 3), () {
       SplashServices().hasLoggedIn(authNotifier).then((value) {
         if (value) {
           NotificationServices.getFirebaseMessagingToken(authNotifier);
-          Navigator.pushReplacementNamed(context, '/home');
+          Navigator.pushNamed(context, '/home');
+          inSplashScreen = false;
         } else {
-          Navigator.pushReplacementNamed(context, '/chooseUserType');
+          Navigator.pushNamed(context, '/chooseUserType');
+          inSplashScreen = false;
         }
       });
     });
+  }
+
+  void showFormDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("No internet connection"),
+            content: const Text("Please connect to the internet to continue"),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  if (await connectivity.checkConnectivity() !=
+                          ConnectivityResult.none &&
+                      isAlertSet) {
+                    if (context.mounted) {
+                      Navigator.pop(context);
+                      isAlertSet = false;
+                      log("alert set $isAlertSet");
+                      if (inSplashScreen) {
+                        moveForward();
+                      }
+                    }
+                  }
+                },
+                child: const Text("OK"),
+              ),
+            ],
+          );
+        });
   }
 
   @override
